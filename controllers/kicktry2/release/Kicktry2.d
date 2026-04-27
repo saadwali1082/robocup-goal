6383fd2b574ccad07e0053ba2f229d3eba2cbb68
saadwali1082 <saadwalimuhammad1082@gmail.com>
2026-04-27 11:37:53 +0500

    Add files via upload

---
+++ controllers/kicktry2/release/Kicktry2.d
@@ -0,0 +1,8 @@
+build/release/Kicktry2.o: Kicktry2.c \
+ C:\Users\Daniyal\ Naeem\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Daniyal\ Naeem\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Daniyal\ Naeem\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Daniyal\ Naeem\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\Daniyal\ Naeem\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\Daniyal\ Naeem\AppData\Local\Programs\Webots/include/controller/c/webots/utils/motion.h \
+ C:/Users/Daniyal\ Naeem/AppData/Local/Programs/Webots/include/controller/c/webots/types.h
