7fc674b0a85bb06c27f3905720a72759fab1a5be
saadwali1082 <saadwalimuhammad1082@gmail.com>
2026-04-27 11:36:02 +0500

    Add files via upload

---
+++ controllers/kickball/build/relese/kickball.d
@@ -0,0 +1,9 @@
+build/release/kickball.o: kickball.c \
+ D:\Webots/include/controller/c/webots/robot.h \
+ D:\Webots/include/controller/c/webots/types.h \
+ D:\Webots/include/controller/c/webots/nodes.h \
+ D:\Webots/include/controller/c/webots/utils/motion.h \
+ D:/Webots/include/controller/c/webots/types.h \
+ D:\Webots/include/controller/c/webots/camera.h \
+ D:\Webots/include/controller/c/webots/camera_recognition_object.h \
+ D:\Webots/include/controller/c/webots/motor.h
